89dc39ffe5a3f9fd00e7df56c6c3d30f3b551120
EC2 Default User <ec2-user@ip-172-31-45-202.eu-north-1.compute.internal>
2026-06-09 22:17:25 +0000

    Add Terraform configuration files4

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = "i-03009d19133983dbd"
+}
+
+output "instance_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = "172.31.45.202"
+}
+
